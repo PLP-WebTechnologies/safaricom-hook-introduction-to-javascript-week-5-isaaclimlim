95ee6c9d74685e83eab0bfc5c294e9478ec1888c
isaaclimlim <isaacerukudi@gmail.com>
2024-12-23 12:36:56 +0300

    DOM assignment completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Variables and Data Types */
+/* 1. Variables and Data Types */
 const myName = 'Isaac Z. Schlueter';
 let age = 22;
 let country = 'Germany';
@@ -23,8 +23,8 @@ if (haveID === true) {
 
 console.log(myInfo)
 
-/* Operators */
- 
+/* 2. Operators */
+/* 
 const num1 = Number(prompt('Enter the first number: '));
 const num2 = Number(prompt('Enter the second number: '));
 const operator = prompt('Enter the operator (+, -, *, /): ');
@@ -55,6 +55,59 @@ if (operator === '+') {
     const result = num1 / num2;
     console.log(`Result: ${result}`);
 }
+*/
+
+/* 3. Functions */
+const greetings = document.querySelector('h4');
+greetings.innerHTML = greetUser('John Lema');
+ function greetUser(name) {
+    return `Hello ${name}, Welcome to Programming!`;
+ }
+
+ /* Part 2: JavaScript Control Structures
+            2.1:If Statements */
+const vote = document.querySelector('h2');
+vote.innerHTML = eligibleToVote();
+function eligibleToVote() {
+    const age = Number(prompt('Enter your age: '));
+    if (age >= 18) {
+        return 'You are eligible to vote';
+    } else {
+        return 'You are not eligible to vote';
+    }
+}
+
+
+
+/* List-of-numbers */
+const numbers = document.getElementById('List');
+numbers.innerHTML = countNumbers();
+function countNumbers() {
+    for (let i=1; i<=10; i++) {
+        const newList = document.createElement('li');
+        newList.textContent = i;
+        numbers.appendChild(newList);
+        
+    }
+}
+
+/* INTRODUCTION TO DOM */
+/* Selecting and Modifying HTML Elements */
+
+const heading = document.querySelector('h1');
+heading.textContent = 'Javascript in Action!';
+
+const newParagraph = document.getElementById('dynamic-content');
+const paragraph = document.createElement('p');
+paragraph.innerHTML = 'This content was added dynamically using JavaScript.';
+newParagraph.appendChild(paragraph);
+
+
+
+
+
+
+ 
 
 
 
